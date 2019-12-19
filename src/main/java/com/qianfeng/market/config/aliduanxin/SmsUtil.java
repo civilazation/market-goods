f57e223f21be9ad7a9edcef5123340729858c05e
Administrator <237377116@qq.com>
2019-12-19 11:02:32 +0800

    我的分支测试，合并代码

--- src/main/java/com/qianfeng/market/config/aliduanxin/SmsUtil.java
+++ src/main/java/com/qianfeng/market/config/aliduanxin/SmsUtil.java
@@ -30,7 +30,6 @@ public class SmsUtil {
     static final String product = "Dysmsapi";
     //产品域名,开发者无需替换
     static final String domain = "dysmsapi.aliyuncs.com";
-    private String s;
 
     // TODO 此处需要替换成开发者自己的AK(在阿里云访问控制台寻找)
     static final String accessKeyId = "LTAI48o4lOvBGMD0";
